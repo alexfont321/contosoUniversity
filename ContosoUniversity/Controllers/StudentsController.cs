8346c4ba9df6606dc394be9d96d7c3a3d171fae3
unknown <alexfont321@gmail.com>
2018-11-26 14:32:43 -0600

    starting crud

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -34,7 +34,12 @@ namespace ContosoUniversity.Controllers
             }
 
             var student = await _context.Students
-                .FirstOrDefaultAsync(m => m.ID == id);
+        .Include(s => s.Enrollments)
+            .ThenInclude(e => e.Course)
+        .AsNoTracking()
+        .SingleOrDefaultAsync(m => m.ID == id);
+
+      
             if (student == null)
             {
                 return NotFound();
